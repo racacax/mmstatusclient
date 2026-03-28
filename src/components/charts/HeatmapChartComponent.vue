0d3c78af1d004ec13a691f878583d63331f20aab
racacax <racacax@gmail.com>
2026-03-28 21:20:14 +0100

    changed heatmap colors

--- src/components/charts/HeatmapChartComponent.vue
+++ src/components/charts/HeatmapChartComponent.vue
@@ -14,39 +14,84 @@ const props = defineProps<{
   colorAxisMax?: number
 }>()
 
-const DAY_NAMES = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+const LIGHT_STOPS: [number, string][] = [
+  [0, '#f7f7f7'],
+  [0.25, '#fdcc8a'],
+  [0.6, '#fc8d59'],
+  [1, '#b30000']
+]
+
+const DARK_STOPS: [number, string][] = [
+  [0, '#1a1a1a'],
+  [0.25, '#5a2d00'],
+  [0.6, '#a03000'],
+  [1, '#cc2200']
+]
+
+function isDark() {
+  return document.body.getAttribute('data-bs-theme') === 'dark'
+}
+
+function renderColors(chart: any) {
+  const series = chart.series?.[0]
+  if (!series?.points) return
+  series.points.forEach((point: any) => {
+    if (point.graphic && point.color) {
+      point.graphic.element.style.fill = point.color
+    }
+  })
+}
 
 function formatData() {
+  const dark = isDark()
+  const stops = dark ? DARK_STOPS : LIGHT_STOPS
+  const labelColor = dark ? '#d6d6d6' : '#333333'
+  const gridColor = dark ? '#474747' : '#e6e6e6'
+
   return {
     chart: {
       type: 'heatmap',
-      styledMode: true,
+      styledMode: false,
       marginTop: 20,
-      marginBottom: 80
+      marginBottom: 80,
+      backgroundColor: 'transparent',
+      events: {
+        render: function (this: any) {
+          renderColors(this)
+        }
+      }
     },
     title: { text: '' },
     xAxis: {
-      categories: props.xCategories
+      categories: props.xCategories,
+      labels: { style: { color: labelColor } },
+      lineColor: gridColor,
+      tickColor: gridColor
     },
     yAxis: {
       categories: props.yCategories,
       title: null,
-      reversed: false
+      reversed: false,
+      labels: { style: { color: labelColor } },
+      gridLineColor: gridColor
     },
     colorAxis: {
       min: 0,
       max: props.colorAxisMax,
-      minColor: 'var(--heatmap-min)',
-      maxColor: 'var(--heatmap-max)'
+      stops,
+      labels: { style: { color: labelColor } }
     },
     legend: {
       align: 'right',
       layout: 'vertical',
       margin: 0,
       verticalAlign: 'top',
-      symbolHeight: 280
+      symbolHeight: 280,
+      itemStyle: { color: labelColor }
     },
     tooltip: {
+      backgroundColor: dark ? '#393939' : '#f7f7f7',
+      style: { color: labelColor },
       formatter: function (this: any) {
         return `<b>${props.yCategories[this.point.y]}</b> on <b>${props.xCategories[this.point.x]}</b>: <b>${this.point.value}</b> games`
       }
@@ -58,7 +103,7 @@ function formatData() {
         data: props.data,
         dataLabels: {
           enabled: true,
-          color: 'var(--heatmap-label)'
+          style: { color: labelColor, textOutline: 'none' }
         }
       }
     ]
